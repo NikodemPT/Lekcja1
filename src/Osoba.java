494e60ff57a6f1248c20f0dd4188adf002b2bef4
NikodemPT <nikodem.bak@uczen.mechaniktg.pl>
2025-09-05 11:53:57 +0200

    Add files via upload

---
+++ src/Osoba.java
@@ -0,0 +1,41 @@
+public abstract class Osoba {
+    // abstract -> nie można utworzyć obiektu z tej klasy
+    private String imie;
+    private  String nazwisko;
+
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public Osoba(){
+        imie = "Edward";
+        nazwisko = "Niziurski";
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public Osoba(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Osoba{" +
+                "imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                '}';
+    }
+}
+
+
